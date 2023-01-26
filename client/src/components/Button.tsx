1b040629828e9afd6958be833d48c3b9cdbc2900
szgyuszi <szgybence@gmail.com>
2023-01-26 14:46:18 +0100

    feat: implement custom button

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import { DetailedHTMLProps, forwardRef, ButtonHTMLAttributes } from "react";
+
+export const Button = forwardRef<
+  HTMLButtonElement,
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <button
+      className={`py-2 px-4  w-full text-center text-xl text-bold text-white bg-blue-500 rounded outline-none hover:bg-blue-400 focus:bg-blue-400 focus:border focus:border-black disabled:bg-gray-500 transition-colors ${className}`}
+      {...rest}
+      ref={ref}
+    >
+      {children}
+    </button>
+  );
+});
