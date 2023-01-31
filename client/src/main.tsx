961a29a728a9672ef9af2f2ccf670aa8fe5c11bd
szgyuszi <szgybence@gmail.com>
2023-01-31 17:53:21 +0100

    refactor: delete unused imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./input.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
