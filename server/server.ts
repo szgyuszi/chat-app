87d2b546f03e4660f97b6b5448d17086b8ae30ca
szgyuszi <szgybence@gmail.com>
2023-01-31 17:55:54 +0100

    feat: implement socket io

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,42 @@
 import { config } from "dotenv";
+import { Socket } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 config();
 
-import fastify from "fastify";
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
 
-const app = fastify();
+const server = http.createServer(app);
 
-app.listen({ port: parseInt(process.env.PORT!) });
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on(
+  "connection",
+  (socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap>) => {
+    console.log(`User connected to ${socket.id}`);
+
+    socket.on("joinRoom", (roomId: string) => {
+      socket.join(roomId);
+      console.log(`User: ${socket.id} joined room: ${roomId}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`User disconnected from ${socket.id}`);
+    });
+  }
+);
+
+const PORT = process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
