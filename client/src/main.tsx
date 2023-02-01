48fca235610929fb5f689768ede3f4aad5d412b9
szgyuszi <szgybence@gmail.com>
2023-02-01 16:15:17 +0100

    refactor: delete react strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,5 @@ import "./input.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
