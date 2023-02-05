f15e6357e0a8b0c02d1fa45b7a0ac0e517b3e864
szgyuszi <szgybence@gmail.com>
2023-02-05 11:42:18 +0100

    feat: add utility file for kepp track rooms

---
+++ client/src/components/Room.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type RoomPropsType = {
+  room: {
+    roomId: string;
+    users: number;
+  };
+};
+
+const Room = ({ room }: RoomPropsType) => {
+  return (
+    <div key={room.roomId}>
+      {room.roomId} : {room.users}
+    </div>
+  );
+};
+
+export default Room;
